89013faecd94ac2b08071396eb5e781d6ce87729
Arpan Garg <arpangarg93@gmail.com>
2016-03-08 16:52:20 -0500

    update ui / refactor

--- foodtrucks/views.py
+++ foodtrucks/views.py
@@ -4,7 +4,5 @@ from models import TruckFrequency
 
 def display_page(request):
 	vendor_list = TruckFrequency.objects.all()
-	#vendor_list = {}
-	#for truck in truck_list:
-	#	vendor_list[truck.vendor] = truck.frequency
+
 	return render(request, 'index.html', {'vendor_list': vendor_list})
