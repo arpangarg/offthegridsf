31a69d60735673d6482a9153addcf05bedb7cb2b
Arpan Garg <arpangarg93@gmail.com>
2016-03-09 02:49:37 -0500

    did the front end to show popularity

--- foodtrucks/views.py
+++ foodtrucks/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from models import TruckFrequency
+from models import TruckFrequency, TruckProperties
 
 
 def display_page(request):
-	vendor_list = TruckFrequency.objects.all()
+	truck_list = TruckFrequency.objects.all()
 
-	return render(request, 'index.html', {'vendor_list': vendor_list})
+	popular_trucks = []
+	max_freq = truck_list[0].frequency
+
+	#This could be implemented much better with relationships rather than running a
+	#query on the database each time.
+	for truck in truck_list:
+		try:
+			truckP = TruckProperties.objects.get(vendor=truck.vendor)
+			popular_trucks.append({
+				'vendor': truck.vendor,
+				'frequency': truck.frequency,
+				'url': truckP.url,
+				'img_url': truckP.img_url,
+				'food_types': truckP.food_types,
+				'popularity': str(100*truck.frequency / max_freq) + ' %'
+			})
+
+		except TruckProperties.DoesNotExist:
+			continue
+
+	return render(request, 'index.html', {'popular_trucks': popular_trucks})
