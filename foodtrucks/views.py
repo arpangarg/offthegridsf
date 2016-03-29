816a568809ba768d09c6182594085f4f8feb3462
Arpan Garg <arpangarg93@gmail.com>
2016-03-28 22:18:34 -0400

    add some diagrams

--- foodtrucks/views.py
+++ foodtrucks/views.py
@@ -26,3 +26,11 @@ def display_page(request):
 			continue
 
 	return render(request, 'index.html', {'popular_trucks': popular_trucks})
+
+def show_diagrams(request, diagram):
+	if (diagram == '360pi_crawl'):
+		return render(request, 'show_diagram.html', {'img_src': '360pi_crawl.png'})
+	elif (diagram == 'amazon_reviews'):
+		return render(request, 'show_diagram.html', {'img_src': 'amazon_reviews.png'})
+	else:
+		return display_page(request)
--- offthegridsf/urls.py
+++ offthegridsf/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from foodtrucks.views import display_page
+from foodtrucks.views import display_page, show_diagrams
 
 urlpatterns = patterns('',
     url(r'^$', display_page),
+    url(r'^(?P<diagram>\w+)/$', show_diagrams),
 )
